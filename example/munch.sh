b4bf5f263c019db1105dbfe7dd1ebc5c0765323d
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-01-23 11:33:50 +0900

    Run muncher a few times when build testing

--- example/munch.sh
+++ example/munch.sh
@@ -1,2 +1,20 @@
+echo "=== Test 1 ==="
 muncher --input sprites/green --output output/background --flip
+echo "=== Test 2 ==="
 muncher --input sprites/square --output output/foreground --flip
+echo "=== No input ==="
+echo ""
+muncher --output output/foreground --flip
+echo ""
+echo "=== No output ==="
+echo ""
+muncher --input sprites/square --flip
+echo ""
+echo "=== Directory not found ==="
+echo ""
+muncher --input unknown --output output/foreground --flip
+echo ""
+echo "=== Texture packer error ==="
+echo ""
+muncher --input sprites-big --output output/foreground --flip
+echo ""
